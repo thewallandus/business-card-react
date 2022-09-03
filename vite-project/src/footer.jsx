6c6b65529e4ae21f279b9ac052929d2c84aa4a68
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-09-03 10:45:40 +0530

    footer fixed

--- vite-project/src/footer.jsx
+++ vite-project/src/footer.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { ReactDOM } from "react";
-
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
-
+import './footer.css'
 
 function Footer() {
     return (
-        <div>
-            <h6>will solve later</h6>
+        <div className="footer">
+            <a href="https://twitter.com/thewallandthem"><i className="fa-brands fa-xl fa-square-twitter footer--icon"></i></a>
+            <a href="https://www.instagram.com/thewallandthem/"><i className="fa-brands fa-xl fa-square-instagram footer--icon"></i></a>
+            <a href="https://github.com/thewallandus"><i className="fa-brands fa-xl fa-square-github"></i></a>
         </div>
     )
 }
